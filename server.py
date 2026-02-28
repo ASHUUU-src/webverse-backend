4c527859af484ef88f9b794d93ca1548b5d4b8eb
ASHUUU-src <reetajha1998@gmail.com>
2026-02-28 18:06:40 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import random
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -27,4 +28,5 @@ def add_quote():
     return jsonify({"message": "Quote added successfully"})
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
